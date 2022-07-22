8c00f2be847797c2df907d988c32b95331455d21
User <User@DESKTOP-FT49PKV>
2022-07-22 15:28:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplicationFibo1/ConsoleApplicationFibo1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int fibonachi(int number) {
+    if (number == 0)return 0;
+    else if (number == 1)return 1;
+    else
+    {
+        return fibonachi(number - 1) + fibonachi(number - 2);
+    }
+}
+int main()
+{
+    int num = 0;
+    cout << "Write your number\n";
+    cin >> num;
+    int res = fibonachi(num);
+    cout << "result -> " << res << endl;
+}
